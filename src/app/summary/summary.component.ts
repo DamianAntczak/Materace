0af574efcc29c23f80cd2f4398c3d64772249bb9
damiana <d_antczak@op.pl>
2020-02-06 23:41:52 +0100

    Summary update

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,8 +1,10 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {KeyValue} from '@angular/common';
 import {Router} from '@angular/router';
-import { faSearch, faPlay } from '@fortawesome/free-solid-svg-icons';
+import {faSearch, faPlay} from '@fortawesome/free-solid-svg-icons';
 import {DataService} from '../data.service';
+import {faPrint} from '@fortawesome/free-solid-svg-icons/faPrint';
+import {faRedo} from '@fortawesome/free-solid-svg-icons/faRedo';
 
 
 @Component({
@@ -17,22 +19,30 @@ export class SummaryComponent implements OnInit {
 
   faSearch = faSearch;
   faPlay = faPlay;
+  faPrint = faPrint;
+  faRedo = faRedo;
+
   link = 'https://www.youtube.com/embed/1_-GMcbhMx8';
 
-  constructor(private router: Router, private dataService: DataService) { }
+  constructor(private router: Router, private dataService: DataService) {
+  }
 
   ngOnInit() {
   }
 
   valueDescOrder = (a: KeyValue<string, number>, b: KeyValue<string, number>): number => {
     return a.value - b.value;
-  }
+  };
 
   playVideo(selectedMattress) {
     let data = this.dataService.getDataAboutMattress(selectedMattress);
     console.log(data);
     this.link = data[2];
     this.showEmbed = true;
+  }
+
+  getMattressData(selectedMattress) {
+    return this.dataService.getDataAboutMattress(selectedMattress);
   }
 
   restart() {
